98f990957313602875f9ba8692f8b224660436e1
Oguzhan Topgul <oguzhantopgul@gmail.com>
2015-11-05 16:59:01 +0300

    AppManipulator v0.2

---
+++ appmanipulator.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python
+
+__author__ = 'Oguzhan Topgul'
+
+from subprocess import call
+import argparse
+import fileinput
+import fnmatch
+import os
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+parser = argparse.ArgumentParser(description='AppManipulator APK Repackager')
+parser.add_argument('-i','--input', help='Input APK file',required=True)
+parser.add_argument('-o','--output',help='Output folder', required=True)
+parser.add_argument('-f','--file', help='File to Manipulate',required=True)
+parser.add_argument('-s','--search', help='Text To Search',required=True)
+parser.add_argument('-r','--replace', help='Text To Replace',required=True)
+args = parser.parse_args()
+
+fileToSearch = args.file
+textToSearch = args.search
+textToReplace = args.replace
+appToBesigned = os.path.normcase(args.output + "/dist/" + args.input)
+
+print bcolors.OKGREEN + "AppManipulator Output [*] Disassemling APK..." + bcolors.ENDC 
+'''Disassemble App''' 
+call(["apktool", "d", args.input, "-o", args.output])
+
+'''Search for file name'''
+matchedFiles = []
+for root, dirnames, filenames in os.walk(args.output):
+    for filename in fnmatch.filter(filenames, fileToSearch):
+        matchedFiles.append(os.path.join(root, filename))
+
+if not matchedFiles:
+	print "File can not be found!"
+else:
+	print "Folloing files are being replaced"
+	for matched in matchedFiles:
+		print bcolors.OKBLUE + "AppManipulatorOutput [*] File:" + matched + bcolors.ENDC 
+
+print bcolors.OKGREEN + "AppManipulator Output [*] Replacing Strings..." + bcolors.ENDC 
+
+'''Find and Replace'''
+for matchedFile in matchedFiles:
+	f = open(matchedFile,'r')
+	filedata = f.read()
+	f.close()
+
+	newdata = filedata.replace(textToSearch,textToReplace)
+
+	f = open(matchedFile,'w')
+	f.write(newdata)
+	f.close()
+
+print bcolors.OKGREEN + "AppManipulator Output [*] Assembling APK Again..." + bcolors.ENDC 
+
+'''Assemble App Again'''
+call(["apktool", "b", args.output])
+
+print bcolors.OKGREEN + "AppManipulator Output [*] Signing Repackaged APK..." + bcolors.ENDC 
+
+'''Sign App'''
+call(["jarsigner", "-sigalg", "SHA1withRSA", "-digestalg", "SHA1", "-keystore", "testkeystore", "-storepass", "testtest", appToBesigned, "testkey"])
+
+print bcolors.OKGREEN + "AppManipulator Output [*] SUCCESS!" + bcolors.ENDC
+
